a0d41afdecc24ad90a26d9bdbfe31adab93f4b93
Ashley <ashley.horn15@yahoo.com>
2023-04-28 09:53:31 -0500

    Added B and FC Controllers

---
+++ Purrrfect-Cats/Controllers/BreedsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Purrrfect_Cats.Controllers
+{
+    public class BreedsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Purrrfect-Cats/Controllers/FavoriteCats.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Purrrfect_Cats.Controllers
+{
+    public class FavoriteCats : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("FavoriteCats");
+        }
+    }
+}
