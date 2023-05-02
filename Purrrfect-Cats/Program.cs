9606bf4f9565e335e6949bd9ad5b32378f372d6f
Esther Onchieku <estheronchieku@gmail.com>
2023-05-02 11:02:55 -0500

    configuration in Program.cs and appsettings.Development.json

--- Purrrfect-Cats/Program.cs
+++ Purrrfect-Cats/Program.cs
@@ -4,16 +4,12 @@ using Purrrfect_Cats.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//var connectionString = "server=localhost;user=catfinder2023;password=catfinder2023;database=catfinder";
-//var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
-
-//builder.Services.AddDbContext<caDbContext>(dbContextOptions => dbContextOptions.UseMySql(connectionString, serverVersion));
 
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseMySql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
