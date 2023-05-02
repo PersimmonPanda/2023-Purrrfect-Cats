54b8f8ed29f44eb2315d476d0adbeeb2e115e710
Jose g hernandez <jose_ghj@hotmail.com>
2023-05-02 16:35:37 -0500

    connected to mysql

---
+++ Purrrfect-Cats/Migrations/20230502212339_catfinder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Purrrfect_Cats.Migrations
+{
+    public partial class catfinder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
