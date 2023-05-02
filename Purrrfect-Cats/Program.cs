a2e0ea6102637c47e2307daa7c9883d25c2c31cf
Esther Onchieku <estheronchieku@gmail.com>
2023-05-02 14:20:13 -0500

    program.cs congiguration

--- Purrrfect-Cats/Program.cs
+++ Purrrfect-Cats/Program.cs
@@ -10,8 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 var connectionString = "server=localhost;user=catfinder2023;password=catfinder2023;database=catfinder";
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(connectionString));
+    options.UseMySql(connectionString, serverVersion));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
