8a58881dcfb78cd502d72059ab880bfbc8c92894
imjose1 <jose_ghj@hotmail.com>
2023-05-04 21:50:32 -0700

    finally done connected to the esternal api

--- Purrrfect-Cats/Controllers/BreedsController.cs
+++ Purrrfect-Cats/Controllers/BreedsController.cs
@@ -13,6 +13,7 @@ namespace Purrrfect_Cats.Controllers
 
             List<SelectListItem> LstBreed = new List<SelectListItem>();
             CatBreedModel catBreed = new CatBreedModel();
+            ListCatsBreedsModel catList = new ListCatsBreedsModel();
             var client = new HttpClient();
             client.DefaultRequestHeaders.Add("x-api-key", "live_F9pFgfTNQsm0mJfBg0FpALoycqilewVeMw8LPpcsmqTZpsydTTRjvhINAQxvTyRE");
 
@@ -38,11 +39,12 @@ namespace Purrrfect_Cats.Controllers
                     if (LstBreed != null)
                     {
                         catBreed.LstBreed = LstBreed;
+                        catList.LstBreeds = catBreed.LstBreed;
                     }
 
                 }
 
-                return View(catBreed);
+                return View(catList);
 
             }
 
@@ -53,9 +55,12 @@ namespace Purrrfect_Cats.Controllers
         [HttpPost]
         public async Task<IActionResult> GetCatByBreed()
         {
-            CatInfoModel catInfo = new CatInfoModel();
+            //CatInfoModel catInfo = new CatInfoModel();
+
             List<BreedTypeModel> LstOneCatBreed = new List<BreedTypeModel>();
             BreedTypeModel oneBreed = new BreedTypeModel();
+            ListCatsBreedsModel selectedBreed = new ListCatsBreedsModel();
+
             var client = new HttpClient();
             string valorSeleccionado = Request.Form["breedId"];
             var response = await client.GetAsync("https://api.thecatapi.com/v1/images/search?limit=5&breed_ids=" + valorSeleccionado + "&api_key=live_F9pFgfTNQsm0mJfBg0FpALoycqilewVeMw8LPpcsmqTZpsydTTRjvhINAQxvTyRE");
@@ -70,7 +75,15 @@ namespace Purrrfect_Cats.Controllers
 
                     foreach (var catBreed in getOneBreed)
                     {
-                        catInfo = new CatInfoModel();
+
+                        selectedBreed = new ListCatsBreedsModel();
+                        selectedBreed.Id = catBreed.Id;
+                        selectedBreed.Url = catBreed.Url;
+                        selectedBreed.Width = catBreed.Width;
+                        selectedBreed.Height = catBreed.Height;
+                        selectedBreed.Breeds = catBreed.Breeds; 
+
+
                         oneBreed = new BreedTypeModel();
                         oneBreed.Id = catBreed.Id;
                         oneBreed.Url = catBreed.Url;
@@ -83,13 +96,55 @@ namespace Purrrfect_Cats.Controllers
 
                     if (getOneBreed != null)
                     {
-                        oneBreed.LstOneCatBreed = LstOneCatBreed;
+                        selectedBreed.SelectedBreed = LstOneCatBreed;
 
                     }
-                    return View("Index", oneBreed);
+                    selectedBreed.LstBreeds = await ListOfBreeds();
+                    return View("Breeds", selectedBreed);
                 }
             }
             return View("Error");
+        }
+        public async Task<List<SelectListItem>> ListOfBreeds()
+        {
+
+            List<SelectListItem> LstBreed = new List<SelectListItem>();
+            CatBreedModel catBreed = new CatBreedModel();
+
+            ListCatsBreedsModel catList = new ListCatsBreedsModel();
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", "live_F9pFgfTNQsm0mJfBg0FpALoycqilewVeMw8LPpcsmqTZpsydTTRjvhINAQxvTyRE");
+
+            var response = await client.GetAsync("https://api.thecatapi.com/v1/breeds");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var breedsJson = await response.Content.ReadAsStringAsync();
+                var breeds = JsonConvert.DeserializeObject<List<CatBreedModel>>(breedsJson);
+
+                if (breeds != null)
+                {
+                    foreach (var breed in breeds)
+                    {
+                        LstBreed.Add(new SelectListItem
+                        {
+                            Value = breed.Id,
+                            Text = breed.Name,
+
+                        });
+
+                    }
+                    if (LstBreed != null)
+                    {
+                        catBreed.LstBreed = LstBreed;
+                        catList.LstBreeds = catBreed.LstBreed;
+                    }
+
+                }
+
+            }
+            return LstBreed;
+
         }
     }
 
