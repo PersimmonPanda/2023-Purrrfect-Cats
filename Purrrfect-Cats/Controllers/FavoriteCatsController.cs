3b1ff3f4530a3ed53563164655f60522a10bec27
Jose g hernandez <jose_ghj@hotmail.com>
2023-05-08 13:53:38 -0500

    sttling breed page

--- Purrrfect-Cats/Controllers/FavoriteCatsController.cs
+++ Purrrfect-Cats/Controllers/FavoriteCatsController.cs
@@ -4,10 +4,10 @@ namespace Purrrfect_Cats.Controllers
 {
     public class FavoriteCatsController : Controller
     {
-        [HttpPost]
+        
         public IActionResult Index(string selectedBreed)
         {
-            string valorSeleccionado = Request.Form["hola"];
+            
 
             var catList = selectedBreed;
             return View("FavoriteCats");
