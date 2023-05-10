7b94f82967a3c39c612f279b444c6f47fca2f08c
Jose g hernandez <jose_ghj@hotmail.com>
2023-05-10 10:38:46 -0500

    pulling pdate

--- Purrrfect-Cats/Models/BreedTypeModel.cs
+++ Purrrfect-Cats/Models/BreedTypeModel.cs
@@ -10,6 +10,7 @@ namespace Purrrfect_Cats.Models
     {
         public string Id { get; set; }
         public string Url { get; set; }
+      
         public int Width { get; set; }
         public int Height { get; set; }
 
